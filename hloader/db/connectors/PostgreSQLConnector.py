f2c6beb6b31833e93a8a61745786fe7de11bdab1
Anirudha Bose <anirudha.bose@cern.ch>
2015-07-01 14:47:52 +0200

    Implement a connector for PostgreSQL

--- hloader/db/connectors/PostgreSQLConnector.py
+++ hloader/db/connectors/PostgreSQLConnector.py
@@ -1,4 +1,22 @@
+import psycopg2
+import sys
+
 class PostgreSQLConnector:
+    def __init__(self, host, dbname, user, password):
+        try:
+            self.connection = psycopg2.connect("host='%s' dbname='%s' user='%s' password='%s'"%(host, dbname, user, password))
+        except:
+            print "Unable to connect to the Postgres server"
+            sys.exit()
+        self.cursor = self.connection.cursor()
+        self.cursor.execute('SHOW server_version')
+        version = self.cursor.fetchone()[0]
+        print "Successfully connected to the PostgreSQL Server (%s)"%(version)
+        print "Host: ", host
+        print "Database: ", dbname
+        print "User: ", user
+
+
     def get_servers(self):
         pass
 
