8721e328a1b708dd3260c47f2511223d1a0a90aa
Anirudha Bose <anirudha.bose@cern.ch>
2015-07-10 11:27:34 +0200

    Implement a basic interval scheduler

--- hloader/entities/Transfer.py
+++ hloader/entities/Transfer.py
@@ -1,17 +1,29 @@
-from enum import Enum
-
-__author__ = 'dstein'
+from datetime import datetime
 
 
-class Transfer:
+class Transfer(object):
     transfer_id = None
     job_id = None
-    transfer_status = None
+    transfer_created = None
     transfer_start = None
+    transfer_status = None
     transfer_last_update = None
 
-    class Status(Enum):
-        WAITING = "waiting"
-        STARTED = "started"
-        SUCCEEDED = "succeeded"
-        # TODO collect status values
+    def __init__(self, _transfer, job):
+        self.transfer_id = _transfer.id
+        self.job_id = job.job_id
+        self.transfer_created = str(datetime.now())
+        self.transfer_start = str(_transfer.next_run_time)
+        self.transfer_status = Status.STARTED
+        self.transfer_last_updated = str(datetime.now())
+
+
+class Status(object):
+    # Status codes for transfers
+    # STARTED   -> 0
+    # RUNNING   -> 1
+    # WAITING   -> 2
+    # SUCCEEDED -> 3
+    # FAILED    -> 4
+
+    STARTED, RUNNING, WAITING, SUCCEEDED, FAILED = range(5)
--- hloader/schedule/schedulers/SimpleScheduler.py
+++ hloader/schedule/schedulers/SimpleScheduler.py
@@ -1,1 +1,62 @@
-__author__ = 'dstein'
+from datetime import datetime
+import time
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
+
+from hloader.entities.Transfer import Transfer
+
+
+class APScheduler(object):
+    def __init__(self):
+        settings = {
+            'jobstore': {
+                # Keep the schedule information in an encrypted SQLite local store.
+                'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
+            },
+
+            # TODO: Avoid hardcoding ThreadPoolExecutor and ProcessPoolExecutor
+            'executors': {
+                'default': ThreadPoolExecutor(20),
+                'processpool': ProcessPoolExecutor(5)
+            },
+
+            'job_defaults': {
+                # TODO: Investigate if we need to coalesce missed jobs
+                'coalesce': False,
+                'max_instances': 3
+            },
+
+            'timezone': "Europe/Zurich"
+        }
+
+        print("Initializing APScheduler")
+        self.scheduler = BackgroundScheduler(jobstores=settings['jobstore'],
+                                             executors=settings['executors'],
+                                             job_defaults=settings['job_defaults'],
+                                             timezone=settings['timezone'])
+
+        print("Starting the scheduling daemon")
+        self.scheduler.start()
+
+
+def start_transfer(scheduler_instance):
+    _transfer = scheduler_instance.scheduler.add_job(tick, 'interval', seconds=2)
+    transfer = Transfer(job, _transfer)
+
+
+def tick():
+    # This is dummy job
+    print('Tick! The time is: %s' % datetime.now())
+
+
+if __name__ == '__main__':
+    # Wait for 5 seconds and start the scheduler
+    time.sleep(5)
+    scheduler_instance = APScheduler()
+
+    start_transfer(scheduler_instance)
+    while True:
+        # TODO: Fine tune this setting to decrease CPU busy waiting
+        time.sleep(60)
