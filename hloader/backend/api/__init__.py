f98419a1195a8a35748de331f22eb99b982e213a
Daniel Stein <daniel.stein@cern.ch>
2015-08-05 15:22:29 +0200

    The API should be available from the outside

--- hloader/backend/api/__init__.py
+++ hloader/backend/api/__init__.py
@@ -1,6 +1,13 @@
 from __future__ import absolute_import
 from flask import Flask
+from flask_cors import CORS
 
 __author__ = 'dstein'
 
 app = Flask(__name__)
+
+CORS(app, resources={
+    r"/api/*": {
+        "origins": "*"
+    }
+})
